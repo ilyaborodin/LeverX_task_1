80c6d285f4c5080164d5e8a629cd7bbad9413d23
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-13 17:43:38 +0300

    Реализовал работу класса RoomWorker

--- RoomWorker.py
+++ RoomWorker.py
@@ -6,3 +6,16 @@ class RoomWorkerABC(ABC):
     @abstractmethod
     def merge_lists_of_students_and_rooms(self, students: list, rooms: list) -> list:
         pass
+
+
+class RoomWorker(RoomWorkerABC):
+    def merge_lists_of_students_and_rooms(self, students: list, rooms: list) -> list:
+        for student in students:
+            room_of_student = student["room"]
+            if "students" in rooms[room_of_student].keys():
+                del student["room"]
+                rooms[room_of_student]["students"].append(student)
+            else:
+                del student["room"]
+                rooms[room_of_student]["students"] = [student, ]
+        return rooms
